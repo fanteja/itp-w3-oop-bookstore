1393eff275fe00354f19f941ef6a1b2be4ec881f
Teja Vadlamudi <teja.vadlamudi@uber.com>
2017-04-07 02:25:48 +0000

    initial commit

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,46 @@
 class Bookstore(object):
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.book_list = []
+        
+    def add_book(self, book):
+        self.book_list.append(book)
+    def get_books(self):
+        return self.book_list
+    
+    def search_books(self,author=None, title=None):
+        if author != None:
+            result = []
+            for book in self.book_list:
+               if author == book.author:
+                   result.append(book)
+            return result
+                   
+        else:
+            result =[]
+            for book in self.book_list:
+                if title.lower() in book.title.lower():
+                    result.append(book)
+            return result
+        
 
 
 class Author(object):
-    pass
+    def __init__(self,name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.book_list = []
+
+    def add_books(self, book):
+        self.book_list.append(book)
+        
+    def get_books(self):
+        return self.book_list 
+    
 
 
 class Book(object):
-    pass
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+        author.add_books(self)
